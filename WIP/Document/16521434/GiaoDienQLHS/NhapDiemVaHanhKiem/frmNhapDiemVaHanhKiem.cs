ea4043bffd42441224afec22ffee346f5f7e66f5
ThuViet <37130098+ThuViet@users.noreply.github.com>
2018-05-07 21:18:32 +0700

    Giao Dien
    Việt gửi giao diện của Việt đã chỉnh sửa vs của Duyên luôn nha, trong file 16521434 của Việt à.

---
+++ WIP/Document/16521434/GiaoDienQLHS/NhapDiemVaHanhKiem/frmNhapDiemVaHanhKiem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NhapDiemVaHanhKiem
+{
+    public partial class frmNhapDiemVaHanhKiem : Form
+    {
+        public frmNhapDiemVaHanhKiem()
+        {
+            InitializeComponent();
+        }
+
+        private void nhậpĐiểmToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nhậpĐiểmToolStripMenuItem1_Click_1(object sender, EventArgs e)
+        {
+            frmNhapDiem f1 = new frmNhapDiem();
+            f1.MdiParent = this;
+            f1.Show();
+        }
+
+        private void nhậpHạnhKiểmToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            frmNhapHanhKiem f2 = new frmNhapHanhKiem();
+            f2.MdiParent = this;
+            f2.Show();
+        }
+    }
+}
---
+++ WIP/Document/16521434/GiaoDienQLHS/NhapDiemVaHanhKiem/frmNhapHanhKiem.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace NhapDiemVaHanhKiem
+{
+    partial class frmNhapHanhKiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "2014 - 2015",
+            "2015 - 2016",
+            "2016 - 2017",
+            "2017 - 2018",
+            "2018 - 2019"});
+            this.comboBox4.Location = new System.Drawing.Point(154, 22);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 28);
+            this.comboBox4.TabIndex = 30;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "10A1",
+            "10A2",
+            "10A3",
+            "11A1",
+            "11A2",
+            "11A3",
+            "12A1",
+            "12A2",
+            "12A3"});
+            this.comboBox3.Location = new System.Drawing.Point(154, 129);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 28);
+            this.comboBox3.TabIndex = 29;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "I",
+            "II"});
+            this.comboBox1.Location = new System.Drawing.Point(154, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Học Kỳ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 20);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Lớp";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Năm Học";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10,
+            this.columnHeader11});
+            this.listView1.Location = new System.Drawing.Point(55, 237);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(872, 295);
+            this.listView1.TabIndex = 32;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "STT";
+            this.columnHeader1.Width = 41;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Mã HS";
+            this.columnHeader2.Width = 64;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Họ Tên";
+            this.columnHeader3.Width = 71;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Điểm Miệng";
+            this.columnHeader4.Width = 92;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Điểm 15\' L1";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Điểm 15\' L2";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Điểm 15\' L3";
+            this.columnHeader7.Width = 96;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Điểm 1 Tiết L1";
+            this.columnHeader8.Width = 115;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Điểm 1 Tiết L2";
+            this.columnHeader9.Width = 116;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Điểm 1 Tiết L3";
+            this.columnHeader10.Width = 116;
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "Điểm Học Kỳ";
+            this.columnHeader11.Width = 106;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(387, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 20);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "BẢNG HẠNH KIỂM";
+            // 
+            // frmNhapHanhKiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 544);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmNhapHanhKiem";
+            this.Text = "Nhập Hạnh Kiểm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+        private System.Windows.Forms.Label label5;
+    }
+}
