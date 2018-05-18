c58a0821dc0b488addc19e3d998cb0529ac408c5
tranngocphuongduyen <37219025+tranngocphuongduyen@users.noreply.github.com>
2018-05-18 08:21:02 +0700

    Nhập điểm
    Chưa hoàn thành

---
+++ WIP/Source/Nhập điểm/BUS/BUS/BUS_NHAPDIEM.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAL;
+using DTO;
+
+namespace BUS
+{
+    public class BUS_NHAPDIEM
+    {
+        NHAPDIEM da = new NHAPDIEM();
+        public void TinhDiemTB(BANGDIEM BD)
+        {
+            int dem = 0;
+            float tong = 0;
+            if (BD.KTM1 != null)
+            {
+                dem += 1;
+                tong += BD._KTM1;
+                if (BD.KTM2 != null)
+                {
+                    dem += 1;
+                    tong += BD._KTM2;
+                    if (BD.KTM3 != null)
+                    {
+                        dem += 1;
+                        tong += BD._KTM3;
+                        if (BD.KTM4 != null)
+                        {
+                            dem += 1;
+                            tong += BD._KTM4;
+                            if (BD.KTM5 != null)
+                            {
+                                dem += 1;
+                                tong += BD._KTM5;
+                            }
+                        }
+                    }
+                }
+            }
+
+            float MTB;
+            MTB = tong / dem;
+            tong = MTB;
+            if (tong != 0)
+                dem = 1;
+            if (BD.KT15P1 != null)
+            {
+                dem += 1;
+                tong += BD._KT15P1;
+            }
+            if (BD.KT15P2 != null)
+            {
+                dem += 1;
+                tong += BD._KT15P2;
+            }
+            if (BD.KT15P3 != null)
+            {
+                dem += 1;
+                tong += BD._KT15P3;
+            }
+            if (BD.KT15P4 != null)
+            {
+                dem += 1;
+                tong += BD._KT15P4;
+            }
+            if (BD.KT15P5 != null)
+            {
+                dem += 1;
+                tong += BD._KT15P5;
+            }
+            if (BD.KT15P6 != null)
+            {
+                dem += 1;
+                tong += BD._KT15P6;
+            }
+            if (BD.KT1T1 != null)
+            {
+                dem += 2;
+                tong += BD._KT1T1*2;
+            }
+            if (BD.KT1T2 != null)
+            {
+                dem += 2;
+                tong += BD._KT1T2*2;
+            }
+            if (BD.KT1T3 != null)
+            {
+                dem += 2;
+                tong += BD._KT1T3*2;
+            }
+            if (BD.KT1T4 != null)
+            {
+                dem += 2;
+                tong += BD._KT1T4*2;
+            }
+            if (BD.KT1T5 != null)
+            {
+                dem += 2;
+                tong += BD._KT1T5*2;
+            }
+            if (BD.KT1T6 != null)
+            {
+                dem += 2;
+                tong += BD._KT1T6*2;
+            }
+            if (BD.DIEMTHI != null)
+            {
+                dem += 2;
+                tong += BD._DIEMTHI*3;
+            }
+            BD._DIEMTB = tong / dem;
+        }
+        public DataTable getBangDiemHK1()
+        {
+            return da.getBangDiemHK1();
+        }
+        public DataTable getBangDiemHK2()
+        {
+            return da.getBangDiemHK2();
+        }
+        public void NhapDiemKTM1(BANGDIEM BD)
+        {
+            da.NhapDiemKTM1(BD);
+        }
+
+        public void NhapDiemKTM2(BANGDIEM BD)
+        {
+            da.NhapDiemKTM2(BD);
+        }
+
+        public void NhapDiemKTM3(BANGDIEM BD)
+        {
+            da.NhapDiemKTM3(BD);
+        }
+
+        public void NhapDiemKTM4(BANGDIEM BD)
+        {
+            da.NhapDiemKTM4(BD);   
+        }
+
+        public void NhapDiemKTM5(BANGDIEM BD)
+        {
+            da.NhapDiemKTM5(BD);
+        }
+
+        public void NhapDiemKT15P1(BANGDIEM BD)
+        {
+            da.NhapDiemKT15P1(BD);
+        }
+
+        public void NhapDiemKT15P2(BANGDIEM BD)
+        {
+            da.NhapDiemKT15P2(BD);
+        }
+
+        public void NhapDiemKT15P3(BANGDIEM BD)
+        {
+            da.NhapDiemKT15P3(BD);
+        }
+
+        public void NhapDiemKT15P4(BANGDIEM BD)
+        {
+            da.NhapDiemKT15P4(BD);
+        }
+
+        public void NhapDiemKT15P5(BANGDIEM BD)
+        {
+            da.NhapDiemKT15P5(BD);
+        }
+
+        public void NhapDiemKT15P6(BANGDIEM BD)
+        {
+            da.NhapDiemKT15P6(BD);
+        }
+
+        public void NhapDiemKT1T1(BANGDIEM BD)
+        {
+            da.NhapDiemKT1T1(BD);
+        }
+
+        public void NhapDiemKT1T2(BANGDIEM BD)
+        {
+            da.NhapDiemKT1T2(BD);
+        }
+
+        public void NhapDiemKT1T3(BANGDIEM BD)
+        {
+            da.NhapDiemKT1T3(BD);
+        }
+
+        public void NhapDiemKT1T4(BANGDIEM BD)
+        {
+            da.NhapDiemKT1T4(BD);
+        }
+
+        public void NhapDiemKT1T5(BANGDIEM BD)
+        {
+            da.NhapDiemKT1T5(BD);
+        }
+
+        public void NhapDiemKT1T6(BANGDIEM BD)
+        {
+            da.NhapDiemKT1T6(BD);
+        }
+
+
+        public void NhapDiemThi(BANGDIEM BD)
+        {
+            da.NhapDiemThi(BD);
+        }
+    }
+}
---
+++ WIP/Source/Nhập điểm/DAL/DAL/NHAPDIEM.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using DTO;
+using System.Configuration;
+
+namespace DAL
+{
+    public class NHAPDIEM
+    {
+        public SqlConnection getConnect()
+        {
+            return new SqlConnection(@"Data Source=USER;Initial Catalog=TestNhapDiem;Integrated Security=True");
+        }
+
+        public DataTable getBangDiemHK1()
+        {
+            SqlConnection conn = getConnect();
+            SqlDataAdapter da = new SqlDataAdapter("select HSHS.MAHS, HOTEN, KTM1, KTM2, KTM3, KTM4, KTM5, KT15P1, KT15P2, KT15P3, KT15P4, KT15P5, KT15P6, KT1T1, KT1T2, KT1T3, KT1T4, KT1T5, KT1T6, DIEMTHI, DIEMTB  from HSHS, BANGDIEM    where HSHS.MAHS = BANGDIEM.MAHS, MAHK = 1", conn);
+            DataTable dtBangDiem = new DataTable();
+            da.Fill(dtBangDiem);
+            return dtBangDiem;
+        }
+
+        public DataTable getBangDiemHK2()
+        {
+            SqlConnection conn = getConnect();
+            SqlDataAdapter da = new SqlDataAdapter("select HSHS.MAHS, HOTEN, KTM1, KTM2, KTM3, KTM4, KTM5, KT15P1, KT15P2, KT15P3, KT15P4, KT15P5, KT15P6, KT1T1, KT1T2, KT1T3, KT1T4, KT1T5, KT1T6, DIEMTHI, DIEMTB  from HSHS, BANGDIEM    where HSHS.MAHS = BANGDIEM.MAHS, MAHK = 2", conn);
+            DataTable dtBangDiem = new DataTable();
+            da.Fill(dtBangDiem);
+            return dtBangDiem;
+        }
+        public DataTable getmalop()
+        {
+            SqlConnection conn = getConnect();
+            SqlDataAdapter da = new SqlDataAdapter("select MALOP from BANGDIEM ", conn);
+            DataTable dtBangDiem = new DataTable();
+            da.Fill(dtBangDiem);
+            return dtBangDiem;
+
+        }
+        public void NhapDiemKTM1(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KTM1 = {1}, DIEMTB = {2}", BD.KTM1, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKTM2(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KTM2 = {1}, DIEMTB = {2}", BD.KTM2, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKTM3(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KTM3 = {1}, DIEMTB = {2}", BD.KTM3, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKTM4(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KTM4 = {1}, DIEMTB = {2}", BD.KTM4, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKTM5(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KTM5 = {1}, DIEMTB = {2}", BD.KTM5, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT15P1(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT15P1 = {1}, DIEMTB = {2}", BD.KT15P1, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT15P2(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT15P2 = {1}, DIEMTB = {2}", BD.KT15P2, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT15P3(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT15P3 = {1}, DIEMTB = {2}", BD.KT15P3, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT15P4(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT15P4 = {1}, DIEMTB = {2}", BD.KT15P4, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT15P5(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT15P5 = {1}, DIEMTB = {2}", BD.KT15P5, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT15P6(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT15P6 = {1}, DIEMTB = {2}", BD.KT15P6, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT1T1(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT1T1 = {1}, DIEMTB = {2}", BD.KT1T1, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT1T2(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT1T2 = {1}, DIEMTB = {2}", BD.KT1T2, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT1T3(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT1T3 = {1}, DIEMTB = {2}", BD.KT1T3, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT1T4(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT1T4 = {1}, DIEMTB = {2}", BD.KT1T4, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT1T5(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT1T5 = {1}, DIEMTB = {2}", BD.KT1T5, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void NhapDiemKT1T6(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET KT1T6 = {1}, DIEMTB = {2}", BD.KT1T6, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+        public void NhapDiemThi(BANGDIEM BD)
+        {
+            SqlConnection conn = getConnect();
+            try
+            {
+                conn.Open();
+                string SQL = string.Format("UPDATE BANGDIEM SET DIEMTHI = {1}, DIEMTB = {2}", BD.DIEMTHI, BD.DIEMTB);
+                SqlCommand cmd = new SqlCommand(SQL, conn);
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
