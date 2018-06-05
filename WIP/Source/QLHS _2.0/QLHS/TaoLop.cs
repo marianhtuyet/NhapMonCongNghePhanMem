b5dc401945c7653eb7f7958f2a321ce436178f30
marianhtuyet <33389645+marianhtuyet@users.noreply.github.com>
2018-06-05 14:41:09 +0700

    update_code
    Update code tao lop

--- WIP/Source/QLHS _2.0/QLHS/TaoLop.cs
+++ WIP/Source/QLHS _2.0/QLHS/TaoLop.cs
@@ -148,6 +148,7 @@ namespace GUI
             }
             DSLopCoSan.DataSource = busLopCoSan.getLopHocCoSan(MaNH, MaLop);
             HSChuaCoLop.DataSource = busTaoLop.getDSLop();
+            listmaHS = new List<int>();
         }
 
        
